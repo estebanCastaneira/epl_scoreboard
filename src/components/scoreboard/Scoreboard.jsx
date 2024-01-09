31680e972fd26e0fa4ccba768ab04cd9135e8d2e
estebanCastaneira <estebanc_b@hotmail.com>
2024-01-09 14:37:02 -0300

    Scoreboard component

--- src/components/scoreboard/Scoreboard.jsx
+++ src/components/scoreboard/Scoreboard.jsx
@@ -1,5 +1,12 @@
+import LionLogo from "../lionlogo/LionLogo"
+import Timer from "../timer/Timer"
 function Scoreboard() {
-  return <h2>Salame</h2>
+  return (
+    <div className="flex flex-col items-center justify-center w-[90%]">
+      <LionLogo />
+      <Timer />
+    </div>
+  )
 }
 
 export default Scoreboard
