e82dde81f22ebc9f602fb5f1ba5afa8326f99c34
estebanCastaneira <estebanc_b@hotmail.com>
2024-01-09 14:36:43 -0300

    LionLogo component

---
+++ src/components/lionlogo/LionLogo.jsx
@@ -0,0 +1,10 @@
+import epl_logo from "/img/epl_lion_logo.png"
+function LionLogo() {
+  return (
+    <div className="flex justify-center">
+      <img src={epl_logo} className="w-[30%]" />
+    </div>
+  )
+}
+
+export default LionLogo
