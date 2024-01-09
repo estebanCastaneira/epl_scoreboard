ae86d8d10b0b6dddd07f9b109688df0b4ace515b
estebanCastaneira <estebanc_b@hotmail.com>
2024-01-09 14:37:01 -0300

    Timer component

---
+++ src/components/timer/Timer.jsx
@@ -0,0 +1,11 @@
+function Timer() {
+  return (
+    <div className="timer rounded-b-2xl">
+      <p className="text-white text-5xl text-center mt-5 font-bold tracking-widest px-14">
+        01:33
+      </p>
+    </div>
+  )
+}
+
+export default Timer
