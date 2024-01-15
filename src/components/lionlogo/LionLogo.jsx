7245a0a0790142ba6902e149c6706b0004cad720
estebanCastaneira <estebanc_b@hotmail.com>
2024-01-15 10:42:06 -0300

    Lion logo

--- src/components/lionlogo/LionLogo.jsx
+++ src/components/lionlogo/LionLogo.jsx
@@ -1,8 +1,8 @@
 import epl_logo from "/img/epl_lion_logo.png"
 function LionLogo() {
   return (
-    <div className="flex justify-center">
-      <img src={epl_logo} className="w-[30%]" />
+    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex items-center justify-center">
+      <img src={epl_logo} className="sm:mb-12" />
     </div>
   )
 }
