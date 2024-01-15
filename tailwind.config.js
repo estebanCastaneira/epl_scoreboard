1056db6aa03d0576660ca2f03c79ed78cc8ca5be
estebanCastaneira <estebanc_b@hotmail.com>
2024-01-15 10:40:38 -0300

    styles config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,13 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    extend: {
+      fontSize: { score: "14rem" },
+    },
     fontFamily: {
       radikal: ["Nootype Radikal", "sans-serif"],
     },
   },
+
   plugins: [],
 }
