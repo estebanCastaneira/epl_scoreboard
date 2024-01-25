52b1704e3eef2f19313dc62b158d9187b01c4794
estebanCastaneira <estebanc_b@hotmail.com>
2024-01-25 13:39:36 -0300

    match finished

--- src/components/display/Display.jsx
+++ src/components/display/Display.jsx
@@ -1,10 +1,13 @@
 import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
+import formatTime from "../../functions/formatTime"
 
 function Display() {
   const eveIncidences = useSelector((state) => state.incidences.home.incidences)
   const livIncidences = useSelector((state) => state.incidences.away.incidences)
-  const { isStarted } = useSelector((state) => state.time)
+  const { isStarted, isFinished, time, injuryTime } = useSelector(
+    (state) => state.time
+  )
 
   const [homeInc, setHomeInc] = useState([])
   const [awayInc, setAwayInc] = useState([])
@@ -15,9 +18,9 @@ function Display() {
   }, [eveIncidences, livIncidences])
   return (
     <div
-      className={`h-[38vh] sm:h-[50vh] w-[90%] sm:w-[80%] pb-3 px-3 flex justify-around bg-white bg-blur-lg bg-opacity-20 rounded-lg overscroll-y-contain overflow-auto relative text-white text-xs sm:text-sm ${
+      className={`h-[38vh] sm:h-[50vh] w-[90%] sm:w-[80%]  px-3 flex justify-around bg-white bg-blur-lg bg-opacity-20 rounded-lg overscroll-y-contain overflow-auto relative text-white text-xs sm:text-sm ${
         isStarted ? "pt-10" : "pt-3"
-      }`}
+      } ${isFinished ? "pb-10" : "pb-3"}`}
     >
       <div
         className={`flex flex-col items-center z-10 justify-center absolute top-0 ${
@@ -42,6 +45,19 @@ function Display() {
         {/* {awayInc.map((inc, i) => (
           <p key={i}>inc</p>
         ))} */}
+      </div>
+      <div
+        className={`flex flex-col items-center z-10 justify-center absolute bottom-0 ${
+          !isFinished && "hidden"
+        }`}
+      >
+        <img
+          className="w-6"
+          src="/icons/whistle.png"
+          alt="whistle icon"
+          typeof="png"
+        />
+        <p>{String((time + injuryTime) / 60)}' Match Finished!</p>
       </div>
     </div>
   )
