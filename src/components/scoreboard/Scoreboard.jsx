05b23904eaf3ed70c7d78e648b3cb731430d1646
estebanCastaneira <estebanc_b@hotmail.com>
2024-02-05 13:30:30 -0300

    folder organization change

--- src/components/scoreboard/Scoreboard.jsx
+++ src/components/scoreboard/Scoreboard.jsx
@@ -1,14 +1,14 @@
-import Timer from "./Timer"
+import Timer from "../timer/Timer"
 import TeamsContainer from "../teams/TeamsContainer"
-
 import Notification from "./Notification"
 function Scoreboard() {
   return (
     <div className="flex flex-col items-center justify-center w-[95%] sm:w-[70%] md:w-[60%] lg:w-[50%] pointer-events-none">
       <TeamsContainer />
-      <Timer />
-
-      <Notification />
+      <div>
+        <Timer />
+        <Notification />
+      </div>
     </div>
   )
 }
